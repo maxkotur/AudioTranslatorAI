3e321636ece393ec04e4e0072b41b5386c75ee0f
Max Kotur <33634648+maxkotur@users.noreply.github.com>
2023-09-19 19:53:48 -0400

    Delete mp3tods.py

--- mp3tods.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-import librosa
-
-# # Load an MP3 file
-# audio = AudioSegment.from_mp3("your_audio.mp3")
-
-# # Preprocess the audio if needed (e.g., resampling)
-# audio = audio.set_frame_rate(44100)  # Set sample rate to 44.1 kHz
